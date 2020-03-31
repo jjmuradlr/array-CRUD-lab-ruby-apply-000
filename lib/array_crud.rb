aa161ee5914aebba2a5b8b10cb10f4497e1588fe
John Mura <jjmuradlr@gmail.com>
2020-03-31 02:23:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,9 +30,14 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+<<<<<<< HEAD
   a = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number = 2
   a[2]
+=======
+  a = ["I", "am", "really", "learning"]
+  puts a["am"]
+>>>>>>> 65954af271078e220694d7fe140ecc5968ba171d
 end
 
 def retrieve_first_element_from_array(array)
